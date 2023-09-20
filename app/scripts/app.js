c4e81573fe48ea90761e91862be0292a1e4303fa
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-09-20 21:36:33 +0300

    layout and js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,20 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+// import { DATA } from "./data";
+// import { Test } from "./test";
+
+// const test = new Test(DATA);
 
 $(() => {
 	svg4everybody();
+
+	$(".questions__answer").on("click", function () {
+		$(".answer").css({"display":"block"});
+		$(".questions").css({"display":"none"});
+	});
+
+	$(".answer__button").on("click", function () {
+		$(".answer").css({"display":"none"});
+		$(".questions").css({"display":"block"});
+	});
 });
