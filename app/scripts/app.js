73b11763fdb2688462fda357df6d9d757b3791f6
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-09-21 02:24:00 +0300

    corrected the layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,20 @@
-import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
-// import { DATA } from "./data";
-// import { Test } from "./test";
+import { DATA } from "./data";
+import { Test } from "./test";
+console.log(DATA)
 
-// const test = new Test(DATA);
+const test = new Test(DATA);
 
 $(() => {
-	svg4everybody();
+	test.init();
 
-	$(".questions__answer").on("click", function () {
-		$(".answer").css({"display":"block"});
-		$(".questions").css({"display":"none"});
-	});
+	// $(".questions__answer").on("click", function () {
+	// 	$(".answer").css({"display":"block"});
+	// 	$(".questions").css({"display":"none"});
+	// });
 
-	$(".answer__button").on("click", function () {
-		$(".answer").css({"display":"none"});
-		$(".questions").css({"display":"block"});
-	});
+	// $(".answer__button").on("click", function () {
+	// 	$(".answer").css({"display":"none"});
+	// 	$(".questions").css({"display":"block"});
+	// });
 });
--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,71 +1,84 @@
-// export class Test {
-// 	constructor(data) {
-// 		this.data = data;
-// 		this.questions = data.questions;
-// 		this.results = data.results;
-// 		this.activeIndex = 0;
-// 		this.answers = {
-// 			А: 0,
-// 			Б: 0,
-// 			В: 0,
-// 		};
+export class Test {
+	constructor(data) {
+		this.data = data;
+		console.log(data);
+		this.questions = data.questions;
+		this.results = data.results;
+		this.activeIndex = 0;
+		this.answers = {
+			А: 0,
+			Б: 0,
+			В: 0,
+		};
 
-// 		this.$questionTitle = $(".questions__title-number");
-// 		this.$answerItem = $(".questions__title-text");
-// 		this.$answer = $(".questions__answer");
-//         this.$answerText= $(".questions__answer-text");
+		this.$questionTitle = $(".questions__answer-text");
+		this.$answerItem = $(".questions__answer");
+		this.$answer = $(".questions__answer");
+        this.$answerText= $(".questions__title-number");
 
-// 		this.$resultFrameTitle = $(".answer__text");
-// 	}
+		this.$resultFrameTitle = $(".answer__text");
 
-// 	init() {
-// 		this.handleEvents();
-// 		this.renderQuestion();
-// 	}
+		this.$answerButton = $(".answer__button");
+		// this.$answerTitle = $(".answer__text");
+	}
 
-// 	handleEvents() {
-// 		this.$answer.on("click", (e) => {
-// 			const id = $(e.target).closest(".questions__answer-text").data("id");
-// 			this.answers[id] += 1;
-// 			this.activeIndex += 1;
-// 			if (this.activeIndex === this.questions.length) {
-// 				this.renderResults();
-// 			} else {
-// 				this.renderQuestion();
-// 			}
-// 		});
-// 	}
+	init() {
+		this.handleEvents();
+		this.renderQuestion();
+	}
 
-// 	renderQuestion() {
-// 		const currentQuestion = this.questions[this.activeIndex];
-// 		const { title, answers } = currentQuestion;
-// 		this.$questionCounter.text(this.activeIndex + 1);
-// 		this.$questionTitle.html(title);
-// 		this.$answerItem.each((id, item) => {
-// 			$(item).find(".questions__answer-text").text(answers[id].text);
-// 		});
-// 	}
+	handleEvents() {
+		this.$answer.on("click", (e) => {
+			const id = $(e.target).closest(".questions__answer-text").data("id");
+			this.answers[id] += 1;
+			this.activeIndex += 1;
+			if (this.activeIndex === this.questions.length) {
+				this.renderResults();
+			} 
+		});
 
-	
-// 	getWinner() {
-// 		let count = 0;
-// 		let winner = "";
-// 		for (let key in this.answers) {
-// 			if (this.answers[key] > count) {
-// 				count = this.answers[key];
-// 				winner = key;
-// 			}
-// 		}
-// 		return winner;
-// 	}
+		this.$answerButton.on("click", (e) => {
+			const id = $(e.target).closest(".questions__answer-text").data("id");
+			this.answers[id] += 1;
+			this.activeIndex += 1;
+			if (this.activeIndex === this.questions.length) {
+				this.renderQuestion();
+			} 
+		});
 
-	
-// 	renderResults() {
-// 		const winner = this.getWinner();
-// 		$("body").removeClass("show-test");
-// 		$("body").addClass("show-result");
-// 		const currentResult = this.results.find((item) => item.id === winner);
-// 		const { id, title, info, description, quote, author } = currentResult;
-// 		this.$resultFrameTitle.text(title);
-// 	}
-// }
+	}
+
+
+	renderQuestion() {
+		const currentQuestion = this.questions[this.activeIndex];
+		console.log(123, currentQuestion)
+		const { title, answers } = currentQuestion;
+		this.$questionTitle.html(title);
+		this.$answerItem.each((id, item) => {
+			$(item).find(".questions__answer-text").text(answers[id].text);
+		});
+	}
+
+	getWinner() {
+		let count = 0;
+		let winner = "";
+		for (let key in this.answers) {
+			if (this.answers[key] > count) {
+				count = this.answers[key];
+				winner = key;
+			}
+		}
+		return winner;
+	}
+
+	renderResults() {
+		const winner = this.getWinner();
+		$("body").removeClass("show-test");
+		$("body").addClass("show-result");
+		const currentResult = this.results.find((item) => item.id === winner);
+		const {id,info} = currentResult;
+		this.$resultFrameTitle.text(id);
+		this.$resultFrameTitle.text(info);
+	}
+
+}
